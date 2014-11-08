f0329843a5c66370c85c2c38f809b2d86bb26da0
inbasho <breck.borges@gmail.com>
2014-11-08 01:24:54 -0500

    refactor

--- src/hack14/gui/Constants.java
+++ src/hack14/gui/Constants.java
@@ -5,8 +5,5 @@ package hack14.gui;
  */
 public class Constants {
 
-    public static int windowSizeX = 900;
-    public static int windowSizeY = 600;
-    public static int graphSizeX = 800;
-    public static int graphSizeY = 500;
+
 }
--- src/hack14/gui/UserFrame.java
+++ src/hack14/gui/UserFrame.java
@@ -1,20 +1,32 @@
 package hack14.gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * Created by bean on 11/7/2014.
  */
-public class UserFrame extends JFrame{
+public class UserFrame extends JFrame {
+    private int windowSizeX, windowSizeY, graphSizeX, graphSizeY;
+    private BufferedImage graph;
+    private Graphics graphGraphics;
     public UserFrame() {
+        getScreenSize();
         this.setVisible(true);
         this.setUndecorated(true);
     }
-}
-    private void getScreenSize(){
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        windowSizeX = (int)screenSize.getWidth();
-        windowSizeY = (int)screenSize.getHeight();
-        graphSizeX = windowSizeX - leftLabelSize;
-        graphSizeY = windowSizeY - bottomLabelSize;
+    private void initGraph(){
+        graph = new BufferedImage(windowSizeX,windowSizeY,BufferedImage.TYPE_INT_ARGB);
+        graphGraphics = graph.createGraphics();
+        //drawBackground();
+        this.pack();
     }
+    private void getScreenSize() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        windowSizeX = (int) screenSize.getWidth();
+        windowSizeY = (int) screenSize.getHeight();
+        graphSizeX = windowSizeX / 2;
+        graphSizeY = windowSizeY / 2;
+    }
+}
